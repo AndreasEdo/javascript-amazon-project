f10ac9ba4d09c6f055585c5ae2d9c2946bc9947e
AndreasEdo <andreasedwardputrajatmiko@gmail.com>
2025-02-16 17:04:16 +0700

    import products and cart from products.js and cart.js

--- script/amazon.js
+++ script/amazon.js
@@ -1,4 +1,5 @@
-
+import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
 
 const productGrid = document.getElementById("products-grid");
 products.forEach((product) => {
